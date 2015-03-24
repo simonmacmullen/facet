2207badd8d0cc03cf7168ffbad1ff972d707c524
Simon MacMullen <simon@simonmacm.co.uk>
2015-03-24 12:49:50 +0000

    Simplest possible UI

--- overlay/js/main.js
+++ overlay/js/main.js
@@ -1,1 +1,7 @@
-alert("Hello");
+var facetApp = angular.module('facetApp', []);
+
+facetApp.controller('FacetListCtrl', function ($scope, $http) {
+    $http.get('db.json').success(function(data) {
+        $scope.pix = data;
+    });
+});
